c67eaf821a1dc8e8ff6e9de0dcc1d3496e03a80b
erikhant <erikd.h49@gmail.com>
2022-06-29 23:36:14 +0700

    fix: allows request on http

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -22,9 +21,5 @@
     </head>
     <body class="font-sans antialiased overflow-x-hidden">
         @inertia
-
-        {{-- @env ('local')
-            <script src="http://localhost:8080/js/bundle.js"></script>
-        @endenv --}}
     </body>
 </html>
